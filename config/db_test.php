1004e4af8ec695e6c174075e0745b45dc14f5224
xietian <xie-tian@163.com>
2018-12-30 10:12:13 +0800

    message

--- config/db_test.php
+++ config/db_test.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=salary',
+    'dsn' => 'mysql:host=localhost:33306;dbname=salary',
     'username' => 'root',
-    'password' => '',
+    'password' => 'TYaqWBNHKCXhu78gpbD10P36MJAU0xPB',
     'charset' => 'utf8mb4',
 ];
